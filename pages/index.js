601c7564038c491b2a3ae9a6c9ac565aaacd7572
leollon <ccsapp.tux@gmail.com>
2020-02-05 15:21:53 +0800

    Navigate Between Pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+// pages/index.js
 // Make sure your React Component is the default export.
+
+// Client-Side History Support
+// When you hit the Back button, it navigates the page to the index page
+// entirely via the client; next / link does all the location.history handling for you.
+import Link from 'next/link';
+
 export default function Index() {
     return (
         <div>
-            <p> Hello Next.js </p>
+            <Link href="/about">
+                {/* Adding Link Props */}
+                <a title="About Page">About Page</a>
+            </Link>
+            <p>Hello Next.js</p>
         </div>
     );
 }
